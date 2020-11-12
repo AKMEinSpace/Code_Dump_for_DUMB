c02c37f8ff94e559dc4c37571d432dff09b987a6
AKMEinSpace <60017392+AKMEinSpace@users.noreply.github.com>
2020-11-12 14:37:47 +0100

    func/lista

---
+++ hlp_func.py
@@ -0,0 +1,13 @@
+''' 
+    Funktioner
+
+'''
+def funktionen(x,y):
+    #x=sn, y=ek
+    return x + y
+
+sn = 10
+ek = 5
+
+print(funktionen(sn,ek))
+#prints 15
---
+++ hlp_lista.py
@@ -0,0 +1,22 @@
+'''
+    snabbare sätt att göra listor
+'''
+
+lista = ["na" for i in range(10)]
+
+#print(lista)    
+#["na","na","na","na","na","na","na","na","na","na"]
+
+#nästlad for loop
+lista = [[i for i in range(1,11)] for j in range(5)]
+
+#print(lista)
+#lista = [[1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10]]
+
+lista = [i for i in range(0,101,2)]
+print(lista)
+
+#redigera listor
+
+while True:
+    
