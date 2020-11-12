74508987f46b7bc722985fece58d74b22e1902a7
AKMEinSpace <60017392+AKMEinSpace@users.noreply.github.com>
2020-11-12 15:35:06 +0100

    Create hlp_sort.py

---
+++ hlp_sort.py
@@ -0,0 +1,17 @@
+'''
+    Sorteringsalgoritmer
+'''
+import random
+lista = [random.randint(1,100) for i in range(10)]
+def bubbleSort(lista):
+    while True:
+        swap=0
+        for i in range (0,len(lista)-1):
+            if lista[i] > lista[i+1]:
+                temp=lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1]=temp
+                swap+=1
+                if swap == 0:
+                    break
+print(bubbleSort(lista))
